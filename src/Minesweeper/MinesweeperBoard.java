cb4c7c1452784da1dfd2f9e728d67fcfa569aa85
Simon Reitbauer <simon.reitbauer2019@gmail.com>
2021-03-18 09:54:35 +0100

    Create MinesweeperBoard.java

---
+++ src/Minesweeper/MinesweeperBoard.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Minesweeper;
+
+/**
+ *
+ * @author simon
+ */
+public class MinesweeperBoard {
+    
+    private boolean[][] board;
+    
+    public MinesweeperBoard() {
+        
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}
